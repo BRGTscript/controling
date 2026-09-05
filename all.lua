d810936341fa4f3b36e9f0bb003368488fa49aa5
BRGTscript <papajan.silleza@gmail.com>
2026-09-05 22:49:59 +0800

    Update all.lua

--- all.lua
+++ all.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/BRGTscript/controling/refs/heads/main/script.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BRGTscript/controling/refs/heads/main/script.lua?v=1"))()
